c1d3507802907384d9a34b579765e235989e5399
Bo <bo.robbrecht@student.howest.be>
2020-11-17 17:32:17 +0100

    Added top bar (Includes new Item & Picking method)

--- resources/views/consumer/alter_list.blade.php
+++ resources/views/consumer/alter_list.blade.php
@@ -2,6 +2,21 @@
 
 @section('main')
     <main>
-        <p>AAAAAA It WOOOOOORKS</p>
+        <form>
+            <div>
+                <a href="#"><span class="fas fa-plus-circle"></span>New Item</a>
+                <label>
+                    <span class="fas fa-hand-holding"></span>
+                    Picking Method:
+                    <input type="text" placeholder="As cheap as possible">
+                </label>
+            </div>
+        </form>
+        <article>
+            <h1></h1>
+        </article>
+        <article>
+
+        </article>
     </main>
 @endsection
