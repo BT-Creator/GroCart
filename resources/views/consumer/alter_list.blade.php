54df5248420c8dea2cc66ccca4bc620343f21db9
Bo <bo.robbrecht@student.howest.be>
2020-11-17 17:44:25 +0100

    Added new Items & Items in list

--- resources/views/consumer/alter_list.blade.php
+++ resources/views/consumer/alter_list.blade.php
@@ -11,6 +11,44 @@
                     <input type="text" placeholder="As cheap as possible">
                 </label>
             </div>
+            <article>
+                <h1>Items</h1>
+                <div class="new-items-container">
+                    <section>
+                        <h2>Milk</h2>
+                        <p>Just plain old milk.</p>
+                    </section>
+                    <section>
+                        <h2>Tresors</h2>
+                        <p><span class="list-property">Weight:</span> 1 KG</p>
+                        <p><span class="list-property">Brand:</span> Kellogs</p>
+                    </section>
+                    <section>
+                        <h2>American</h2>
+                        <p><span class="list-property">Weight:</span> 0,200 gram</p>
+                        <p><span class="list-property">Special Notes:</span> With unions</p>
+                    </section>
+                </div>
+            </article>
+            <article>
+                <h1>Items in List</h1>
+                <div class="List-items-container">
+                    <section>
+                        <h2>Milk</h2>
+                        <p>Just plain old milk.</p>
+                    </section>
+                    <section>
+                        <h2>Tresors</h2>
+                        <p><span class="list-property">Weight:</span> 1 KG</p>
+                        <p><span class="list-property">Brand:</span> Kellogs</p>
+                    </section>
+                    <section>
+                        <h2>American</h2>
+                        <p><span class="list-property">Weight:</span> 0,200 gram</p>
+                        <p><span class="list-property">Special Notes:</span> With unions</p>
+                    </section>
+                </div>
+            </article>
         </form>
         <article>
             <h1></h1>
