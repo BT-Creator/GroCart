a71bf62f6ff7414b8b274d88d8cddf3f535cc3ae
Bo <bo.robbrecht@student.howest.be>
2020-11-17 18:26:27 +0100

    Added fields for delivery & store address and Delivery & Medical Notes

--- resources/views/consumer/alter_list.blade.php
+++ resources/views/consumer/alter_list.blade.php
@@ -5,10 +5,10 @@
         <form>
             <div>
                 <a href="#"><span class="fas fa-plus-circle"></span>New Item</a>
-                <label>
+                <label for="picking_method">
                     <span class="fas fa-hand-holding"></span>
                     Picking Method:
-                    <input type="text" placeholder="As cheap as possible">
+                    <input type="text" placeholder="As cheap as possible" name="picking_method">
                 </label>
             </div>
             <article>
@@ -49,10 +49,77 @@
                     </section>
                 </div>
             </article>
+            <article>
+                <h3>Store Address:</h3>
+                <label for="store_address">
+                    Street:
+                    <input type="text" placeholder="street" name="store_address" required="required">
+                </label>
+                <label for="store_number">
+                    Number:
+                    <input type="number" name="store_number" required="required">
+                </label>
+                <label for="store_postal_code">
+                    Postal code:
+                    <input type="number" name="store_postal_code" required="required">
+                </label>
+                <label for="store_city">
+                    City:
+                    <input type="text" name="store_city" required="required">
+                </label>
+                <label for="store_country">
+                    Country:
+                    <input type="text" name="store_country" required="required">
+                </label>
+            </article>
+            <article>
+                <h3>Delivery address:</h3>
+                <label for="delivery_address">
+                    Street:
+                    <input type="text" placeholder="street" name="delivery_address" required="required">
+                </label>
+                <label for="delivery_number">
+                    Number:
+                    <input type="number" name="delivery_number" required="required">
+                </label>
+                <label for="delivery_postal_code">
+                    Postal code:
+                    <input type="number" name="delivery_postal_code" required="required">
+                </label>
+                <label for="delivery_city">
+                    City:
+                    <input type="text" name="delivery_city" required="required">
+                </label>
+                <label for="delivery_country">
+                    Country:
+                    <input type="text" name="delivery_country" required="required">
+                </label>
+            </article>
+            <article>
+                <h3>Delivery Notes</h3>
+                <label for="delivery_notes">
+                    <input type="text" name="delivery_notes">
+                </label>
+            </article>
+            <article>
+                <h3>Medical Notes</h3>
+                <label for="medical_notes">
+                    <input type="text" name="medical_notes">
+                </label>
+            </article>
+            <div>
+                <label for="list_save">
+                    <button type="submit" name="list_save">Save List</button>
+                </label>
+                <label for="list_order">
+                    <button type="button" name="list_order">Order List</button>
+                </label>
+                <label for="list_delete">
+                    <button type="reset" name="list_delete">Delete List</button>
+                </label>
+
+            </div>
         </form>
-        <article>
-            <h1></h1>
-        </article>
         <article>
 
         </article>
