0376d4d34ba261c3bd3053a7f3bbec0a0b344c3f
Bo <bo.robbrecht@student.howest.be>
2020-11-20 17:31:33 +0100

    Fix a small issue where the re-attached eventlistener was of the wrong type

--- public/assets/js/modules/draggable.js
+++ public/assets/js/modules/draggable.js
@@ -2,16 +2,18 @@
 export function drag(e){
     console.log(e)
     console.log("Dragging...")
+    this.style.opacity = '0.4'
 }
 
 export function drop(f){
     console.log(f)
     console.log("Stop dragging object")
+    this.style.opacity = '1'
 }
 
 export function resetDraggableObjects(objects){
     document.querySelectorAll(objects).forEach(item => item.removeEventListener('dragstart', drag))
     document.querySelectorAll(objects).forEach(item => item.removeEventListener('dragend', drop))
-    document.querySelectorAll(objects).forEach(item => item.addEventListener('dragend', drag))
+    document.querySelectorAll(objects).forEach(item => item.addEventListener('dragstart', drag))
     document.querySelectorAll(objects).forEach(item => item.addEventListener('dragend', drop))
 }
