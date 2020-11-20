e6226a5f772584b0a613ee494c1d5217efb49b0e
Bo <bo.robbrecht@student.howest.be>
2020-11-20 17:51:15 +0100

    Able to pick-up the element & store it in a separate value, but not able to drop the element

--- public/assets/js/modules/draggable.js
+++ public/assets/js/modules/draggable.js
@@ -1,14 +1,24 @@
 "use strict";
+let dragElement = ''
+
 export function drag(e){
     console.log(e)
     console.log("Dragging...")
     this.style.opacity = '0.4'
+    dragElement = this
+    e.dataTransfer.effectAllowed = 'move'
+    e.dataTransfer.setData('text/html', this.innerHTML)
+    console.log(dragElement)
 }
 
 export function drop(f){
     console.log(f)
     console.log("Stop dragging object")
+    f.stopPropagation()
     this.style.opacity = '1'
+    f.dataTransfer.dropEffect = 'move'
+    f.target.appendChild(dragElement)
+    return false
 }
 
 export function resetDraggableObjects(objects){
