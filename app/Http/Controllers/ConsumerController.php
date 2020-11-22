22438ddc3a8886f48f8ec99f03413b3819b84504
Bo <bo.robbrecht@student.howest.be>
2020-11-22 15:07:44 +0100

    Completely stuck with isolating the ID's. Stopping for now.

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -8,34 +8,37 @@ use Illuminate\Support\Facades\DB;
 
 class ConsumerController extends Controller
 {
-    function index($id){
+    function index($id)
+    {
         $db_order_ids = DB::table('orders')
-            -> join('items', 'orders.id', '=', 'items.order_id')
-            -> select('orders.id')
-            -> where('orders.user_id', '=', $id)
-            -> where('orders.status', '=', 'draft')
-            -> groupBy('orders.id')
-            -> get();
-        $order_ids = [];
-        $data = DB::table('orders')
-                    -> join('items', 'orders.id', '=', 'items.order_id')
-                    -> select('orders.id', 'items.*')
-                    -> where('orders.user_id', '=', $id)
-                    -> where('orders.status', '=', 'draft')
-                    -> orderByDesc('orders.id')
-                    -> get();
-        foreach (explode('->', $db_order_ids) as $order_id){
-            array_push($order_ids, $order_id);
-        }
-        dd($order_ids);
+            ->join('items', 'orders.id', '=', 'items.order_id')
+            ->select('orders.id')
+            ->where('orders.user_id', '=', $id)
+            ->where('orders.status', '=', 'draft')
+            ->groupBy('orders.id')
+            ->get();
+        $db_order_ids->each(function ($value, $key){
+            $id = collect($value) -> get('id');
+            $data = DB::table('orders')
+                ->join('items', 'orders.id', '=', 'items.order_id')
+                ->select('orders.id', 'items.*')
+                ->where('orders.user_id', '=', $id)
+                ->where('orders.status', '=', 'draft')
+                ->orderByDesc('orders.id')
+                ->get();
+            dd($data, $id, $key);
+        });
+        dd($db_order_ids);
         return view('consumer.lists');
     }
 
-    function openList(){
+    function openList()
+    {
         return view('consumer.alter_list');
     }
 
-    function openProfile(){
+    function openProfile()
+    {
         return view('consumer.profile');
     }
 }
