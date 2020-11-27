3d2ddbd3df50c3a4869c320bae042ff069668a4c
Bo <bo.robbrecht@student.howest.be>
2020-11-27 11:19:46 +0100

    Reworked CSS & template for profile

--- resources/views/consumer/profile.blade.php
+++ resources/views/consumer/profile.blade.php
@@ -7,57 +7,61 @@
 @section('main')
     <main class="profile">
         <article>
-            <h1>Welcome back, <span>User</span></h1>
             <div class="profile-card">
+                <h1>Welcome back, <span>User</span></h1>
                 <figure>
                     <img src="#" alt="Profile Pic">
                     <figcaption>User Name</figcaption>
                 </figure>
-                <section>
-                    <h2>Amount of lists made: <span>40 Lists</span></h2>
-                    <h2>Items received: <span>1534 items</span></h2>
-                    <h2>Joined since
-                        <time datetime="2019-12-01 07:00">2019-12-01</time>
-                    </h2>
-                    <form>
-                        <input type="file">
-                        <input type="submit" value="Upload" class="button">
-                    </form>
-                </section>
             </div>
             <section>
-                <h2>Amount of items per list</h2>
-            </section>
-            <section>
-                <h2>Line graph of goods received per order</h2>
-            </section>
-            <section>
-                <h2>Line Graph for time Taken for order to delivery per order</h2>
+                <h2>Amount of lists made: <span>40 Lists</span></h2>
+                <h2>Items received: <span>1534 items</span></h2>
+                <h2>Joined since
+                    <time datetime="2019-12-01 07:00">2019-12-01</time>
+                </h2>
+                <form>
+                    <input type="file">
+                    <input type="submit" value="Upload" class="button">
+                </form>
             </section>
         </article>
+        <article>
+            <div class="graph">
+                <h2>Amount of items per list</h2>
+            </div>
+            <div class="graph">
+                <h2>Line graph of goods received per order</h2>
+            </div>
+            <div class="graph">
+                <h2>Line Graph for time Taken for order to delivery per order</h2>
+            </div>
+        </article>
         <aside>
-            <h2>Ongoing orders</h2>
-            @foreach($ongoing_orders as $id => $order)
-                <section class="profile-order">
-                    <span class="fas fa-shopping-basket"></span>
-                    <div>
-                        <h3>Order {{$id}}</h3>
-                        <p>Total Items: <span>{{count($order)}}</span></p>
-                    </div>
-                </section>
-            @endforeach
-        </aside>
-        <aside>
-            <h2>Completed orders</h2>
-            @foreach($completed_orders as $id => $order)
-                <section class="profile-order">
-                    <span class="fas fa-shopping-basket"></span>
-                    <div>
-                        <h3>Order {{$id}}</h3>
-                        <p>Total Items: <span>{{count($order)}}</span></p>
-                    </div>
-                </section>
-            @endforeach
+            <div>
+                <h2>Ongoing orders</h2>
+                @foreach($ongoing_orders as $id => $order)
+                    <section class="profile-order">
+                        <span class="fas fa-shopping-basket"></span>
+                        <div>
+                            <h3>Order {{$id}}</h3>
+                            <p>Total Items: <span>{{count($order)}}</span></p>
+                        </div>
+                    </section>
+                @endforeach
+            </div>
+            <div>
+                <h2>Completed orders</h2>
+                @foreach($completed_orders as $id => $order)
+                    <section class="profile-order">
+                        <span class="fas fa-shopping-basket"></span>
+                        <div>
+                            <h3>Order {{$id}}</h3>
+                            <p>Total Items: <span>{{count($order)}}</span></p>
+                        </div>
+                    </section>
+                @endforeach
+            </div>
         </aside>
     </main>
 @endsection
