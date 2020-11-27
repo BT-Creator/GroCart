6cc24fcfe3b56855d3ced5462be5154adfcc1bd8
Bo <bo.robbrecht@student.howest.be>
2020-11-27 17:12:06 +0100

    Tried to draw the line graph, but doesn't work

--- public/assets/js/graphs.js
+++ public/assets/js/graphs.js
@@ -11,22 +11,30 @@ function scriptLoader() {
 }
 
 function itemGraph() {
-    d3.json(url + "/consumer/1/orders").then(function (data){
+    d3.json(url + "/consumer/1/orders").then(function (data) {
         let dataXY = data.map((object) => {
             let id = object.id
             let amount = object.amount
-            return {"id": id, "amount":amount}
+            return {"id": id, "amount": amount}
         })
         addSVG(itemContainer)
         const yScale = d3.scaleLinear().domain([0, getMax(data)])
-        const xScale = d3.scaleLinear().domain([0, data.length])
+        const xScale = d3.scaleLinear().domain([data.map((object) => {return object.id})])
         console.log(yScale, xScale)
+        itemContainer.select('svg')
+            .append('g')
+            .selectAll('g')
+            .data(dataXY)
+            .join('g')
+            .append("path")
     })
 
     function getMax(data) {
         let amounts = data.map((object) => {
             return object.amount
         });
-        return amounts.reduce((a,b) => {return Math.max(a,b)})
+        return amounts.reduce((a, b) => {
+            return Math.max(a, b)
+        })
     }
 }
