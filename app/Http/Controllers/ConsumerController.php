ed76b9608bf2aeea0344c007ffbb98eb8aeba0dc
Bo <bo.robbrecht@student.howest.be>
2020-11-29 16:49:31 +0100

    Exported queryies to Query.php
    Again, better readability.

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -1,24 +1,15 @@
 <?php
-
-
 namespace App\Http\Controllers;
 include 'Utilities/Format.php';
 include 'Utilities/Validation.php';
+include 'Utilities/Query.php';
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 class ConsumerController extends Controller
 {
-    //Main Functions
     function index($id)
     {
-        $data = DB::table('orders')
-            ->join('items', 'orders.id', '=', 'items.order_id')
-            ->select('orders.id', 'items.*')
-            ->where('orders.user_id', '=', $id)
-            ->where('orders.status', '=', 'draft')
-            ->orderByDesc('orders.id')
-            ->get();
+        $data = getOrdersAndItems($id);
         $res = formatByOrders($data);
         return view('consumer.lists', ['orders' => $res]);
     }
@@ -30,40 +21,23 @@ class ConsumerController extends Controller
 
     function openExistingList($id, $list)
     {
-        $items = DB::table('orders')
-            ->join('items', 'orders.id', '=', 'items.order_id')
-            ->select('items.*')
-            ->where('orders.user_id', '=', $id)
-            ->where('orders.id', '=', $list)
-            ->get();
-        $items = formatByOrders($items);
-        $order_details = DB::table('orders')
-            ->join('deliveries', 'orders.delivery_id', '=', 'deliveries.id')
-            ->join('stores', 'orders.store_id', '=', 'stores.id')
-            ->select('orders.id', 'orders.picking_method', 'orders.delivery_notes', 'orders.medical_notes',
-                'deliveries.street as delivery_street', 'deliveries.house_number as delivery_house_number',
-                'deliveries.postal_code as delivery_postal_code', 'deliveries.city as delivery_city',
-                'deliveries.country as delivery_country',
-                'stores.street as store_street', 'stores.house_number as store_house_number',
-                'stores.postal_code as store_postal_code', 'stores.city as store_city', 'stores.country as store_country')
-            ->where('orders.user_id', '=', $id)
-            ->where('orders.id', '=', $list)
-            ->get();
+        $items = formatByOrders(getItemsFromList($id, $list));
+        $order_details = getOrderDetails($id, $list);
         $order_details = collect($order_details->get(0));
         return view('consumer.alter_list', ['details' => $order_details, 'items' => $items]);
     }
 
     function updateExistingList(Request $request, $id, $list) {
         $validated_items = validateItems($request);
-        $items = jsonCollectionToItemArray($validated_items, $list);
+        $items = collect(jsonCollectionToItemArray($validated_items, $list)) -> forget('id') -> toArray();
         $details = validateDetails($request);
         $store_address = validateStoreAddress($request);
         $delivery_address = validateDeliveryAddress($request);
-        $ref = formatByItems(collect(DB::table('items')
+        $ref = collect(formatByItems(collect(DB::table('items')
                                         -> where('order_id', '=', $list)
-                                        -> get()));
-        $this -> updateItemDB($items, $ref);
-        dd($items, $details, $store_address, $delivery_address);
+                                        -> get()))) -> forget('id') -> toArray();
+        $this -> updateItemTable($items, $ref);
+        dd($items, $ref, $details, $store_address, $delivery_address);
     }
 
 
@@ -77,7 +51,7 @@ class ConsumerController extends Controller
     }
 
     //DB Sub-functions
-    private function updateItemDB($items, $ref){
+    private function updateItemTable($items, $ref){
         foreach (array_keys($ref) as $key){
             if(!key_exists($key, $items)){
                 echo "Deleting item $key";
