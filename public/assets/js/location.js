373f8caaed75f525766bf69d2360be7fb3647cf4
Bo <bo.robbrecht@student.howest.be>
2020-12-04 16:02:39 +0100

    Able to show the geographical location of a user on the map.

--- public/assets/js/location.js
+++ public/assets/js/location.js
@@ -17,12 +17,28 @@ function getLocation() {
             target: 'map',
             layers: [
                 new ol.layer.Tile({
+                    source: new ol.source.OSM({
+                        url: 'https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png',
+                        attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>' ],
+                        maxZoom: 18
+                    })
+                }),
+                new ol.layer.Tile({
                     source: new ol.source.OSM()
+                }),
+                new ol.layer.Vector({
+                    source: new ol.source.Vector({
+                        features: [
+                            new ol.Feature({
+                                geometry: new ol.geom.Point(ol.proj.fromLonLat([longitude, latitude]))
+                            })
+                        ]
+                    })
                 })
             ],
             view: new ol.View({
-                center: ol.proj.fromLonLat([0, 0]),
-                zoom: 4
+                center: ol.proj.fromLonLat([longitude, latitude]),
+                zoom: 20
             })
         });
     })
