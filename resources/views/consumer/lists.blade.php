037cf38d4b9050f32b6d36dadbb6268174d958f0
Bo <bo.robbrecht@student.howest.be>
2020-12-04 10:47:40 +0100

    Added a counter-measure against users with no draft lists.

--- resources/views/consumer/lists.blade.php
+++ resources/views/consumer/lists.blade.php
@@ -5,27 +5,34 @@
 @endsection
 
 @section('main')
-    <main id="grocery-lists">
-        @foreach($orders as $order)
+    <main class="grocery-lists">
+        @if(empty(collect(collect($orders) -> first()) -> first()))
+            <article class="empty-lists">
+                <h1>No list found :(</h1>
+                <p>How about creating one?</p>
+                <a class="button" href="{{route('create_list', 1)}}"><span class="fas fa-plus-circle"></span>New List</a>
+            </article>
+        @else
+            @foreach($orders as $order)
             <article class="grocery-lists">
                 <h1>List</h1>
                 <div class="grocery-list-container" >
                     @foreach($order as $item)
-                    <section class="list-item">
-                        <h2>{{str_replace('_', ' ', $item['name'])}}</h2>
-                        @isset($item['brand'])
-                            <p><span class="list-property">Brand:</span>{{str_replace('_', ' ', $item['brand'])}}</p>
-                        @endisset
-                        @isset($item['weight'])
-                            <p><span class="list-property">Weight:</span>{{$item['weight']}}</p>
-                        @endisset
-                        @isset($item['note'])
-                            <p><span class="list-property">Note:</span>{{str_replace('_', ' ', $item['note'])}}</p>
-                        @endisset
-                        @if(!isset($item['brand']) && !isset($item['weight']) && !isset($item['note']))
-                            <p>Just plain old {{$item['name']}}</p>
-                        @endisset
-                    </section>
+                        <section class="list-item">
+                            <h2>{{str_replace('_', ' ', $item['name'])}}</h2>
+                            @isset($item['brand'])
+                                <p><span class="list-property">Brand:</span>{{str_replace('_', ' ', $item['brand'])}}</p>
+                            @endisset
+                            @isset($item['weight'])
+                                <p><span class="list-property">Weight:</span>{{$item['weight']}}</p>
+                            @endisset
+                            @isset($item['note'])
+                                <p><span class="list-property">Note:</span>{{str_replace('_', ' ', $item['note'])}}</p>
+                            @endisset
+                            @if(!isset($item['brand']) && !isset($item['weight']) && !isset($item['note']))
+                                <p>Just plain old {{$item['name']}}</p>
+                            @endisset
+                        </section>
                     @endforeach
                 </div>
                 <div class="grocery-list-options">
@@ -34,7 +41,8 @@
                     <a class="button" href="{{route('make_order', [1, $order[0]['order_id']])}}">Order<span class="fas fa-arrow-right"></span></a>
                 </div>
             </article>
-        @endforeach
+            @endforeach
+        @endif
     </main>
 
 @endsection
