62220fc5a09acb4cb6d92cbf7d9312b7814183c9
Bo <bo.robbrecht@student.howest.be>
2020-12-11 10:23:21 +0100

    Extracted query to separate files. DQL.php contains all queries that retrieves data from the DB dml.php contains all queries that perform a manipulation to a database

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-include 'Utilities/Format.php';
-include 'Utilities/Validation.php';
-include 'Utilities/Query.php';
+include 'Utilities/format.php';
+include 'Utilities/validation.php';
+include 'Utilities/dql.php';
+include 'Utilities/dml.php';
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -53,64 +54,13 @@ class ConsumerController extends Controller
             $store_address = collect(validateStoreAddress($request));
             $delivery_address = collect(validateDeliveryAddress($request));
             $ref = formatByItems(getItemsFromList($id, $list));
-            $this->updateItemTable($items, $ref);
-            $this->updateDeliveryTable($delivery_address, $id, $list);
-            $this->updateStoreTable($store_address, $id, $list);
-            $this->updateOrderTable($details, $list);
+            updateItemTable($items, $ref);
+            updateDeliveryTable($delivery_address, $id, $list);
+            updateStoreTable($store_address, $id, $list);
+            updateOrderTable($details, $list);
             DB::commit();
             return $this->openExistingList($id, $list);
         }
-    }
-
-    private function updateItemTable($items, $ref)
-    {
-        foreach (array_keys($ref) as $key) {
-            if (!key_exists($key, $items)) {
-                DB::table('items')->where('id', '=', $key)->delete();
-            }
-        }
-        foreach ($items as $key => $value) {
-            if ($key < 0) {
-                $new_item = collect($value);
-                DB::table('items')->insert(
-                    ['name' => $new_item->get('name'),
-                        'brand' => $new_item->get('brand'),
-                        'weight' => $new_item->get('weight'),
-                        'note' => $new_item->get('note'),
-                        'order_id' => $new_item->get('order_id')]
-                );
-            }
-        }
-    }
-
-    private function updateDeliveryTable($delivery_address, int $userId, int $listId)
-    {
-        $details = getOrderDetails($userId, $listId);
-        $delivery_id = collect($details[0])->get('delivery_id');
-        DB::table('deliveries')
-            ->where('id', '=', $delivery_id)
-            ->update(['street' => $delivery_address->get('delivery_street'),
-                'house_number' => $delivery_address->get('delivery_number'),
-                'postal_code' => $delivery_address->get('delivery_postal_code'),
-                'city' => $delivery_address->get('delivery_city'),
-                'country' => $delivery_address->get('delivery_country')]);
-    }
-
-    private function updateStoreTable($store_address, $userId, $listId)
-    {
-        $details = getOrderDetails($userId, $listId);
-        $store_id = collect($details[0])->get('store_id');
-        DB::table('stores')->where('id', '=', $store_id)
-            ->update(['street' => $store_address->get('store_street'),
-                'house_number' => $store_address->get('store_number'),
-                'postal_code' => $store_address->get('store_postal_code'),
-                'city' => $store_address->get('store_city'),
-                'country' => $store_address->get('store_country')]);
-    }
-
-    private function updateOrderTable($order_details, $listId)
-    {
-        DB::table('orders')->where('id', '=', $listId)->update($order_details->toArray());
     }
 
     function addList(Request $request, $id): RedirectResponse
@@ -125,46 +75,11 @@ class ConsumerController extends Controller
             $details = collect(validateDetails($request));
             $store_address = collect(validateStoreAddress($request));
             $delivery_address = collect(validateDeliveryAddress($request));
-            $delivery_id = $this->insertDelivery($delivery_address);
-            $store_id = $this->insertStore($store_address);
-            $list_id = $this->insertOrder($details, $delivery_id, $store_id, $id);
-            $this->insertItems($items, $list_id);
+            $delivery_id = insertDelivery($delivery_address);
+            $store_id = insertStore($store_address);
+            $list_id = insertOrder($details, $delivery_id, $store_id, $id);
+            insertItems($items, $list_id);
             return redirect()->route('open_list', [$id, $list_id]);
-        }
-    }
-
-    private function insertDelivery($delivery_address): int
-    {
-        return DB::table('deliveries')->insertGetId(
-            ['street' => $delivery_address->get('delivery_street'),
-                'house_number' => $delivery_address->get('delivery_number'),
-                'postal_code' => $delivery_address->get('delivery_postal_code'),
-                'city' => $delivery_address->get('delivery_city'),
-                'country' => $delivery_address->get('delivery_country')]);
-    }
-
-    private function insertStore($store_address): int
-    {
-        return DB::table('stores')->insertGetId(
-            ['street' => $store_address->get('store_street'),
-                'house_number' => $store_address->get('store_number'),
-                'postal_code' => $store_address->get('store_postal_code'),
-                'city' => $store_address->get('store_city'),
-                'country' => $store_address->get('store_country')]);
-    }
-
-    private function insertOrder($order, int $delivery_id, int $store_id, int $user_id): int
-    {
-        $addon = ['delivery_id' => $delivery_id, 'store_id' => $store_id, 'user_id' => $user_id];
-        return DB::table('orders')->insertGetId(array_merge($order->toArray(), $addon));
-    }
-
-    private function insertItems($items, int $order_id)
-    {
-        foreach ($items as $key => $value) {
-            $item = collect(json_decode($value));
-            $item = $item->forget("id")->put('order_id', $order_id)->toArray();
-            DB::table('items')->insert($item);
         }
     }
 
@@ -206,6 +121,4 @@ class ConsumerController extends Controller
         $order = collect(getOrderDetails($user_id, $order_id)[0]);
         return view('consumer.order', ['details' => $order]);
     }
-
-
 }
